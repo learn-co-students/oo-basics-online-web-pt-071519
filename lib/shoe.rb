85f34604f80681bfcc6743bf432b32cf33be8137
jose luis <cortezluisjose@gmail.com>
2019-08-07 03:38:41 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
-# Make your shoe class here!
+class Shoe# Make your shoe class here!
+attr_accessor :brand, :size, :condition
+attr_reader :material, :color
+def initialize(title)
+  @title=title
+end
+def Adidas
+  @Adidas
+end
+def brand=(brand)
+  @brand=brand
+end 
+def brand 
+  @brand 
+  puts "Nike"
+
+end
+def material=(material)
+  @material=material
+end 
+def material
+  @material
+end
+def brand 
+  @brand
+end
+def cobble 
+  @cobble
+  puts "Your shoe is as good as new!"
+  
+end
+def color=(color)
+  @color=color 
+end 
+def color
+  @color
+end
+
+
+end
+hoe.brand=("new")
