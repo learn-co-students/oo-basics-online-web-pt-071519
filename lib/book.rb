287a3942f105b28c4a12972d56098199f7212030
Renee Sims <info@reneesoul.com>
2019-08-07 09:15:22 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book
+  
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title = "And Then There Were None")
+    @title = title
+    # @author = author
+    # @genre = genre
+    # @page_count = page_count
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
-
+book = Book.new
+book.turn_page
+# Book.title
