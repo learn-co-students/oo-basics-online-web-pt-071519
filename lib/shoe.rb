561db3cad66884f0dbf0d77830598dcd5903daaa
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-15 03:23:41 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,16 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize (brand)
+    @brand=brand
+  end
+
+  def cobble=(cobble)
+    puts "new"
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition="new"
+  end
 end
